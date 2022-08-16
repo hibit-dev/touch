070b1cb21600945b589e5c02c142e0c794d42f59
Tigran Mekinyan <tmekinyan@mail.ru>
2022-08-16 14:13:17 +0200

    Add code

---
+++ src/touch/touch.ino
@@ -0,0 +1,35 @@
+// -------------------------------------------------
+// Copyright (c) 2022 HiBit <https://www.hibit.dev>
+// -------------------------------------------------
+
+#define BUTTON_PIN 4
+
+struct touch {
+  byte wasPressed = LOW;
+  byte isPressed = LOW;
+};
+
+touch touch;
+
+void setup()
+{
+  pinMode(BUTTON_PIN, INPUT);
+
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  touch.isPressed = isTouchPressed(BUTTON_PIN);
+
+  if (touch.wasPressed != touch.isPressed) {
+    Serial.println("Touch pressed");
+  }
+
+  touch.wasPressed = touch.isPressed;
+}
+
+bool isTouchPressed(int pin)
+{
+  return digitalRead(pin) == HIGH;
+}
